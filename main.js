c984f2cddb0176ca8272a7d3bf5c796e39f632fa
JohnC0de <joaocarlos.m014@gmail.com>
2022-06-10 09:59:05 -0300

    update: fixed env variables, added dotenv

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const dotenv = require('dotenv').config()
 const crypto = require('crypto')
 const querystring = require('querystring')
 
@@ -11,14 +12,14 @@ const stateKey = 'spotify_auth_state'
 const info = {
   client_id: process.env.CLIENT_ID,
   client_secret: process.env.CLIENT_SECRET,
-  redirect_uri: 'http://localhost:8888/callback',
+  redirect_uri: process.env.REDIRECT_URI,
   response_type: 'code',
   scope: 'user-read-private user-read-email',
   state: randomState()
 }
 
+console.log(info)
 app.use(cors())
-// .use(cookieParser())
 
 app.get('/callback', function (req, res) {
   res.redirect(
@@ -26,4 +27,4 @@ app.get('/callback', function (req, res) {
   )
 })
 
-app.listen(8888, () => console.log('Listening on 8888'))
+// app.listen(8888, () => console.log('Listening on 8888'))
