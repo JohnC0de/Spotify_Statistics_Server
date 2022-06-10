ff4f13562fa4d41b75eb0455d3f21877e399e8f1
JohnC0de <joaocarlos.m014@gmail.com>
2022-06-10 15:47:47 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const dotenv = require('dotenv').config()
 const crypto = require('crypto')
 const querystring = require('querystring')
 
@@ -8,22 +9,23 @@ const app = express()
 const randomState = () => crypto.randomBytes(8).toString('hex')
 
 const stateKey = 'spotify_auth_state'
-const info = {
-  client_id: process.env.CLIENT_ID,
-  client_secret: process.env.CLIENT_SECRET,
-  redirect_uri: 'http://localhost:8888/callback',
-  response_type: 'code',
-  scope: 'user-read-private user-read-email',
-  state: randomState()
-}
 
 app.use(cors())
 // .use(cookieParser())
 
-app.get('/callback', function (req, res) {
+app.get('/login', function (req, res) {
+  const info = {
+    client_id: process.env.CLIENT_ID,
+    client_secret: process.env.CLIENT_SECRET,
+    redirect_uri: process.env.REDIRECT_URI,
+    response_type: 'code',
+    scope: 'user-read-private user-read-email',
+    state: randomState()
+  }
+
   res.redirect(
-    'https://accounts.spotify.com/authorize?' + querystring.stringify(info)
+    'https://accounts.spotify.com/authorize?' + .stringify(info)
   )
 })
 
-app.listen(8888, () => console.log('Listening on 8888'))
+// app.listen(8888, () => console.log('Listening on 8888'))
